411f881a74cd30a7ee67643fea2576b0760c12b8
Nguyễn Văn Tùng <nguyentungtungtungnguyen@gmail.com>
2021-11-13 09:19:31 +0700

    new require

--- client/src/app/pages/product_name/ProductNameFormDefinition.ts
+++ client/src/app/pages/product_name/ProductNameFormDefinition.ts
@@ -1,8 +1,10 @@
 import Rule from "framework/documents/models/Rule";
+import React from "react";
 
 interface ProductNameFormRef {
     inputName: React.RefObject<any>;
     productTypeOption: React.RefObject<any>;
+    inputVolatility?: React.RefObject<any>;
 }
 
 interface ProductNameFormValidate {
